11d66c7adce64eab5183b30965c1fab754254b52
scalevillain764 <kvyatkovskymaxim7@gmail.com>
2026-08-22 17:42:14 +0300

    feat: add remove request command and handler

---
+++ MediCycle/Application/Requests/RemoveRequest/RemoveRequestCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using Infrastructure.Result;
+namespace Application.Requests.RemoveRequest
+{
+    public record RemoveRequestCommand(Ulid CommandRequest) : IRequest<Result<string>>;
+}
---
+++ MediCycle/Application/Requests/RemoveRequest/RemoveRequestCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using ICurrentUser = Application.Abstractions.ICurrentUser;
+using Infrastructure.Result;
+using Infrastructure;
+using Error = Domain.Enums.ErrorType;
+namespace Application.Requests.RemoveRequest
+{
+    public class RemoveRequestCommandHandler : IRequestHandler<RemoveRequestCommand, Result<string>>
+    {
+        private readonly AppDbContext _context;
+        private readonly ICurrentUser _currentUser;
+        public RemoveRequestCommandHandler(AppDbContext context, ICurrentUser currentUser)
+        {
+            _context = context;
+            _currentUser = currentUser;
+        }
+        public async Task<Result<string>> Handle(RemoveRequestCommand command, CancellationToken token)
+        {
+            var request = await _context.Requests
+                .FindAsync(command.RequestId);
+
+            if (request == null)
+                return Result<string>.Error("Заявка не найдена", Error.NotFound);
+
+            if (request.ClientId != _currentUser.UserId)
+                return Result<string>.Error("Это не ваша заявка", Error.Forbidden);
+
+            if (request.Status == Domain.Enums.RequestStatus.InProgress ||
+                request.Status == Domain.Enums.RequestStatus.Completed)
+                return Result<string>.Error("Заявку отменить нельзя. Перезвоните, пожалуйста, водителю", Error.Conflict);
+
+            request.Status = Domain.Enums.RequestStatus.Cancelled;
+
+            await _context.SaveChangesAsync(token);
+
+            return Result<string>.Success("OK");
+        }
+    }
+}
