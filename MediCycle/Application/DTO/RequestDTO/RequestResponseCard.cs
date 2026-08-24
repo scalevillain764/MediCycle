126dffa50e38963c4c3f19811487f1d072b50341
Max <kvyatkovskymaxim7@gmail.com>
2026-08-24 22:55:56 +0300

    feat: add requestId to requestResponseCard

--- MediCycle/Application/DTO/RequestDTO/RequestResponseCard.cs
+++ MediCycle/Application/DTO/RequestDTO/RequestResponseCard.cs
@@ -1,14 +1,18 @@
 ﻿using Request = Domain.Request;
 namespace Application.DTO.RequestDTO
 {
-    public record RequestResponseCard(string organizationName,
+    public record RequestResponseCard(
+        Ulid RequestId,
+        string organizationName,
         string street, 
         string city,
         string buildingNumber,
         DateTime CreatedAt)
     {
         public RequestResponseCard(Request request)
-            : this(request.Client.OrganizationName,
+            : this(
+                  request.Id,
+                  request.Client.OrganizationName,
                   request.RequestAddress.City,
                   request.RequestAddress.Street,
                   request.RequestAddress.BuildingNumber,
