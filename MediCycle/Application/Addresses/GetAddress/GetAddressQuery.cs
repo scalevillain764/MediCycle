6dbf86d5f9fef5d34792f53c130fc0c276f444ec
Max <kvyatkovskymaxim7@gmail.com>
2026-08-31 17:20:31 +0300

    feat: add GetAddress query and handler

---
+++ MediCycle/Application/Addresses/GetAddress/GetAddressQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using Infrastructure.Result;
+using Application.DTO.AddressDTO;
+namespace Application.Addresses
+{
+    public record GetAddressQuery(Ulid addressId): IRequest<Result<AddressResponse>>;
+}
---
+++ MediCycle/Application/Addresses/GetAddress/GetAddressQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using ICurrentUser = Application.Abstractions.ICurrentUser;
+using AddressResponse = Application.DTO.AddressDTO.AddressResponse;
+using Address = Domain.Address;
+using Infrastructure.Result;
+using Infrastructure;
+using Error = Domain.Enums.ErrorType;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore;
+namespace Application.Addresses
+{
+    public class GetAddressQueryHandler : IRequestHandler<GetAddressQuery, Result<AddressResponse>>
+    {
+        private readonly AppDbContext _context;
+        private readonly ICurrentUser _currentUser;
+        public GetAddressQueryHandler(AppDbContext context, ICurrentUser currentUser)
+        {
+            _context = context;
+            _currentUser = currentUser;
+        }
+        public async Task<Result<AddressResponse>> Handle(GetAddressQuery query, CancellationToken token)
+        {
+            var address = await _context.Addresses
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == query.addressId, token);
+
+            if (address == null)
+                return Result<AddressResponse>.Error("Адрес не найден", Error.NotFound);
+
+            return Result<AddressResponse>.Success(new AddressResponse(address));
+        }
+    }
+}
