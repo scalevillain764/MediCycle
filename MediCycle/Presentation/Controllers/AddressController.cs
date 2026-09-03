49da07d20bd4d903949e19f9fb01f5abc641bfe0
Max <kvyatkovskymaxim7@gmail.com>
2026-09-03 14:59:59 +0300

    feat: add all methods to address controller; add auth controller with no methods

--- MediCycle/Presentation/Controllers/AddressController.cs
+++ MediCycle/Presentation/Controllers/AddressController.cs
@@ -8,7 +8,7 @@ namespace Presentation.Controllers
 {
     [ApiController]
     [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
-    [Authorize(Policy = "ClientOnly")]
+    [Authorize]
     public class AddressController : BaseController
     {
         private readonly IMediator _mediator;
@@ -18,11 +18,30 @@ namespace Presentation.Controllers
         }
 
         [HttpPost]
+        [Authorize(Policy = "ClientOnly")]
         public async Task<IActionResult> AddAddressAsync([FromBody] AddAddressCommand command, CancellationToken token)
             => ProcessResult(await _mediator.Send(command, token));
 
         [HttpPatch("{addressId}/edit")]
+        [Authorize(Policy = "ClientOnly")]
         public async Task<IActionResult> EditAddressAsync([FromQuery] Ulid addressId, [FromBody] EditAddressCommand command, CancellationToken token)
             => ProcessResult(await _mediator.Send(command with { addressId = addressId}, token));
+
+        [HttpGet("{addressId}")]
+        public async Task<IActionResult> GetAdddressByIdAsync([FromQuery] Ulid addressId, CancellationToken token)
+            => ProcessResult(await _mediator.Send(new GetAddressQuery(addressId), token));
+
+        [HttpGet("client/{clientId}")]
+        public async Task<IActionResult> GetAddressesByClientIdAsync([FromQuery] Ulid clientId, CancellationToken token) 
+            => ProcessResult(await _mediator.Send(new GetAddressesByClientIdQuery(clientId), token));
+
+        [HttpGet("my")]
+        public async Task<IActionResult> GetMyAddressesAsync(CancellationToken token)
+            => ProcessResult(await _mediator.Send(new GetAddressesByClientIdQuery(CurrentUserId), token));
+
+        [HttpDelete("{addressId}")]
+        [Authorize(Policy = "ClientOnly")]
+        public async Task<IActionResult> RemoveAddressAsync([FromQuery] Ulid addressId, CancellationToken token)
+            => ProcessResult(await _mediator.Send(new RemoveAddressCommand(addressId), token));
     }
 }
---
+++ MediCycle/Presentation/Controllers/AuthController.cs
@@ -0,0 +1,8 @@
+﻿using IAuthService = Application.Interfaces.IAuthService;
+using Microsoft.AspNetCore.Mvc;
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : Apic
+}
