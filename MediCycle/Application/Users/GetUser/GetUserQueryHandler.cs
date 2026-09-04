acaa297afc2ce6430ff20272a12031e7e1a2ecf6
Max <kvyatkovskymaxim7@gmail.com>
2026-09-04 16:17:10 +0300

    feat: add redis to get user query handler

--- MediCycle/Application/Users/GetUser/GetUserQueryHandler.cs
+++ MediCycle/Application/Users/GetUser/GetUserQueryHandler.cs
@@ -5,30 +5,55 @@ using Infrastructure.Responding;
 using Infrastructure;
 using Domain;
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
+using System.Text.Json;
 namespace Application.Users 
 { 
     public class GetUserQueryHandler : IRequestHandler<GetUserQuery, Result<UserResponse>>
     {
         private readonly AppDbContext _context;
-        public GetUserQueryHandler(AppDbContext context)
+        private readonly ICurrentUser _currentUser;
+        private readonly IDatabase _redis;
+
+        public GetUserQueryHandler(AppDbContext context, IConnectionMultiplexer redis, ICurrentUser currentUser)
         {
             _context = context;
+            _currentUser = currentUser;
+            _redis = redis.GetDatabase();
         }
         public async Task<Result<UserResponse>> Handle(GetUserQuery query, CancellationToken token)
         {
+            if(_currentUser.UserId == query.userId)
+            {
+                var cachedUser = await _redis.StringGetAsync($"user:{_currentUser.UserId}");
+                if(cachedUser.HasValue)
+                {
+                    var deserializedUser = JsonSerializer.Deserialize<UserResponse>((string)cachedUser!);
+
+                    if (deserializedUser != null)
+                        return Result<UserResponse>.Success(deserializedUser);
+                }
+            }
+
             var user = await _context.AllUsers
                 .AsNoTracking()
                 .FirstOrDefaultAsync(x => x.Id == query.userId, token);
 
-            if (user == null)
+            if (user == null) 
                 return Result<UserResponse>.Error("Пользователь не найден", Domain.Enums.ErrorType.NotFound);
 
-            return Result<UserResponse>.Success(user switch
+            UserResponse response = user switch
             {
-                  Worker w => new WorkerResponse(w.Id, w.Login, w.Name, w.Surname, w.Birthday),
-                  Client c => new ClientResponse(c.Id, c.Login, c.OrganizationName),
-                  _ => throw new InvalidOperationException($"Неизвестный тип пользователя: {user.GetType().Name}")
-            });
+                Worker w => new WorkerResponse(w.Id, w.Login, w.Name, w.Surname, w.Birthday),
+                Client c => new ClientResponse(c.Id, c.Login, c.OrganizationName),
+                _ => throw new InvalidOperationException($"Неизвестный тип пользователя: {user.GetType().Name}")
+            };
+
+            var serializedUser = JsonSerializer.Serialize((object)response); 
+
+            await _redis.StringSetAsync($"user:{user.Id}", serializedUser, TimeSpan.FromHours(1));
+
+            return Result<UserResponse>.Success(response);
         }
     }
 }
